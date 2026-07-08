bd145bb648b48279aa9e64743d77538524283cc1
Yasin <yasinpeker43@gmail.com>
2026-07-08 15:22:42 +0300

    applying endpoints with ModelViewSet

--- scheduler/views.py
+++ scheduler/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Schedule,Surgeon,Surgery,AnesthesiaTeam,Constraints,OperationRoom
+from .serializer import ScheduleSerializer,SurgeonSerializer,SurgerySerializer,AnesthasiaTeamSerializer,ConstraintsSerializer,OperationRoomSerializer
 
-# Create your views here.
+class OperationRoomViewSet(viewsets.ModelViewSet):
+    queryset = OperationRoom.objects.all()
+    serializer_class = OperationRoomSerializer
+
