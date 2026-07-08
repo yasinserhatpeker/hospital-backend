0f4460f3c034093bb7ade3c472d6a6853f8061ab
Yasin <yasinpeker43@gmail.com>
2026-07-08 15:42:21 +0300

    endpoints are done for now.

--- scheduler/views.py
+++ scheduler/views.py
@@ -6,3 +6,22 @@ class OperationRoomViewSet(viewsets.ModelViewSet):
     queryset = OperationRoom.objects.all()
     serializer_class = OperationRoomSerializer
 
+class SurgeonViewSet(viewsets.ModelViewSet):
+   queryset = Surgeon.objects.all()
+   serializer_class = SurgeonSerializer
+   
+class SurgeryViewSet(viewsets.ModelViewSet):
+    queryset = Surgery.objects.all()
+    serializer_class = SurgerySerializer
+    
+class ScheduleViewSet(viewsets.ModelViewSet):
+    queryset=Schedule.objects.select_related('room', 'surgeon', 'surgery','team').all()
+    serializer_class = ScheduleSerializer
+
+class AnesthesiaTeamViewSet(viewsets.ModelViewSet):
+    queryset = AnesthesiaTeam.objects.all()
+    serializer_class = AnesthasiaTeamSerializer
+    
+class ConstraintsViewSet(viewsets.ModelViewSet):
+    queryset = Constraints.objects.all()
+    serializer_class = ConstraintsSerializer
