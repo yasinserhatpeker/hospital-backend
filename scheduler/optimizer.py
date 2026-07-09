3d5479212f9ec3e2867c8cc86cecde5358b4a16b
Yasin <yasinpeker43@gmail.com>
2026-07-09 11:15:22 +0300

    surgeryOptimizer class is created & start and end date are configured

--- scheduler/optimizer.py
+++ scheduler/optimizer.py
@@ -2,3 +2,19 @@ from .models import AnesthesiaTeam,Surgeon,Surgery,Schedule,OperationRoom,Constr
 from datetime import datetime,timedelta
 from django.db import transaction
 
+class SurgeryOptimizer:
+    def __init__(self,start_date_str,end_date_str):
+        self.start_date_str = datetime.strftime(start_date_str,"%Y-%m-%d").date()
+        self.end_date_str = datetime.strftime(end_date_str,"%Y-%m-%d").date()
+        
+        self.active_constraints = self._load_constraints()
+        self.total_daily_slots = 10
+        
+        self.schedule_grid= {}
+        self.surgeon_tracker = {}
+        self.team_tracker = {}
+        
+        self.rooms = list(OperationRoom.objects.all())
+        self.teams = list(AnesthesiaTeam.objects.all())
+        
+        
